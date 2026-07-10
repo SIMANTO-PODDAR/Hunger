cb7622c8d8a9fc07d63651051f4300fe13a408fc
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-07-10 14:04:12 +0600

    Update Navbar UI

--- src/Sections/Navbar.tsx
+++ src/Sections/Navbar.tsx
@@ -1,7 +1,78 @@
+import Image from "next/image";
+import Logo from "../../public/hunger.png";
+import { AiOutlineLogin, AiOutlineLogout } from "react-icons/ai";
+import Link from "next/link";
+
 const Navbar = () => {
+    // const loggedIn = true;
+    const loggedIn = false;
+
     return (
-        <div>
-            Navbar
+        <div className="bg-base-100 shadow-md">
+            <div className="navbar container mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+
+                        <ul
+                            tabIndex={-1}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><Link href='/'>Home</Link></li>
+                            <li><a>All Foods</a></li>
+                            <li><a>About</a></li>
+                            {
+                                loggedIn && (
+                                    <>
+                                        <li><a>Add Food</a></li>
+                                        <li><a>Manage Foods</a></li>
+                                    </>
+                                )
+                            }
+                        </ul>
+                    </div>
+
+                    <div className="sm:navbar-start">
+                        <Image src={Logo} alt='Hunger Logo' height={40} title="Hunger" />
+                    </div>
+                </div>
+
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link href='/'>Home</Link></li>
+                        <li><a>All Foods</a></li>
+                        <li><a>About</a></li>
+                        {
+                            loggedIn && (
+                                <>
+                                    <li><a>Add Food</a></li>
+                                    <li><a>Manage Foods</a></li>
+                                </>
+                            )
+                        }
+                    </ul>
+                </div>
+
+                {
+                    loggedIn ? (
+                        <div className="navbar-end gap-0.5">
+                            <button className="btn btn-sm btn-error font-bold text-white flex gap-2 items-center" >
+                                Logout
+                                <span className="text-xl"><AiOutlineLogout /></span>
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="navbar-end">
+                            <Link href={'/login'} className="btn btn-sm btn-accent font-bold text-white flex gap-2 items-center">
+                                Login
+                                <span className="text-xl rotate-180"><AiOutlineLogin /></span>
+                            </Link>
+                        </div>
+                    )
+                }
+
+            </div>
         </div>
     );
 };
